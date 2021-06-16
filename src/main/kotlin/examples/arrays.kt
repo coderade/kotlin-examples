8f809ffdda8dc116446568ff9b4a1913c4e7cf8b
Valdeci Gomes <valfilho@amazon.com>
2021-06-16 17:41:15 -0300

    Add arrays examples

---
+++ src/main/kotlin/examples/arrays.kt
@@ -0,0 +1,41 @@
+package main.kotlin.examples
+
+data class Student(val firstName: String, val grade: Int)
+
+
+fun main() {
+    val things = arrayOf(1, 2, 3, "one", "two") //mixed types array
+//    things[0] = "zero"
+
+    for(thing in things){
+        println(thing)
+    }
+
+    val students = arrayOf<Student>(
+        Student("Valdeci", 10),
+        Student("Jess", 10)
+    )
+
+    students[1] = Student("Jhon", 5)
+
+    for(student in students){
+        println(student)
+    }
+
+
+    val fives = Array(5, ::fiver)
+    fives.forEach { println(it) }
+
+
+    println("fives has ${fives.size} elements")
+    println("indices ${fives.indices}")
+    println("fives last index ${fives.lastIndex}")
+
+
+    val pInts = intArrayOf(1,2,3,4)
+
+}
+
+fun fiver(idx: Int): Int{
+    return idx * 5
+}
